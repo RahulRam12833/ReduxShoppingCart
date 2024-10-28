a821a56b089db79adee417b4487588bc0db8d77c
Rahul <ramr12833@gmail.com>
2024-10-28 18:42:22 +1030

    Created a basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
 import Product from './components/Product';
+import Dashboard from './components/Dashboard';
+import RootLayout from './components/RootLayout';
+import Cart from './components/Cart';
 
 function App() {
+
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>} >
+      <Route index element={<Dashboard/>}></Route>
+      <Route path="/cart" element={<Cart/>}></Route>
+    </Route>
+  ))
   return (
     <div className="App">
-      <Product/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
