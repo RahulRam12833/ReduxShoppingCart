dc4bac0aa1b4a56f1fea6f1d138ba74140046759
Rahul <ramr12833@gmail.com>
2024-10-28 18:25:57 +1030

    Created a Products layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Product = () => {
 
@@ -9,10 +11,33 @@ const Product = () => {
         .then(data=>data.json())
         .then(result=>getProducts(result))
     },[])
+
+    const cards=products.map(product=>(
+        <div className='col-md-3' style={{marginBottom:'8px'}}>
+            <Card key={product.id} className='h-100'>
+                <div className='text-center'>
+                    <Card.Img variant="top" src={product.image} style={{width:'100px',height:'130px'}}/>
+
+                </div>
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+          AUD{product.price}
+        </Card.Text>
+        
+      </Card.Body>
+      <Card.Footer style={{background:'white'}}>
+      <Button variant="primary">Add to Cart</Button>
+      </Card.Footer>
+    </Card>
+        </div>
+    ))
   return (
     <>
     <h1>Product Dashboard</h1>
-    {JSON.stringify(products)}\
+    <div className='row'>
+        {cards}
+    </div>
     </>
   )
 }
