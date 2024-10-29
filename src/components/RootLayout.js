2ccb1690a1d5a4b5ee3c799944b44577ab1e11a8
Rahul <ramr12833@gmail.com>
2024-10-29 19:32:00 +1030

    Created basic routing

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from './NavBar'
 
 const RootLayout = () => {
   return (
     <>
-    <div>Navigation</div>
+    <div><NavBar/></div>
     <Outlet/>
     </>
   )
